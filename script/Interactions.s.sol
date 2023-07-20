827a7b1f600915a779ad227a3e7b2b9b64fbe467
st4rbrain <cosmofi@wsl.com>
2023-07-21 02:04:31 +0530

    created interaction script and added makefile

---
+++ script/Interactions.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+
+contract MintBasicNFT is Script {
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function mintNFTOnContract(address basicNFT) public {
+        vm.startBroadcast();
+        BasicNFT(basicNFT).mintNFT(PUG_URI);
+        vm.stopBroadcast();
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("BasicNFT", block.chainid);
+        mintNFTOnContract(mostRecentlyDeployed);
+    }
+}
--- test/BasicNFTTest.t.sol
+++ test/BasicNFTTest.t.sol
@@ -10,6 +10,10 @@ contract BasicNFTTest is Test {
     DeployBasicNFT public deployer;
     BasicNFT public basicNFT;
 
+    address public USER = makeAddr("user");
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
     function setUp() external {
         deployer = new DeployBasicNFT();
         basicNFT = deployer.run();
@@ -20,6 +24,13 @@ contract BasicNFTTest is Test {
         string memory actualName = basicNFT.name();
 
         assert(keccak256(abi.encodePacked(expectedName)) == keccak256(abi.encodePacked(actualName)));
+    }
+
+    function testCanMintAndHaveABalance() public {
+        vm.prank(USER);
+        basicNFT.mintNFT(PUG_URI);
+        assert(basicNFT.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(PUG_URI)) == keccak256(abi.encodePacked(basicNFT.tokenURI(0))));
     }
 
 }
